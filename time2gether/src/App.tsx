962f3ba8ad56468eca003d89d35b9c2a4a8f9e80
kakiy11 <grigoriev.elisei2007@gmail.com>
2026-05-21 18:58:43 +0300

    fix bug

--- time2gether/src/App.tsx
+++ time2gether/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+// App.tsx
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
 import Auth from './auth/auth';
@@ -15,37 +15,35 @@ const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => 
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Auth />} />
-        <Route path="/register" element={<Registration />} />
-        <Route 
-          path="/Main" 
-          element={
-            <PrivateRoute>
-              <Main />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/profile" 
-          element={
-            <PrivateRoute>
-              <Profile />
-            </PrivateRoute>
-          } 
-        />
-        <Route 
-          path="/history" 
-          element={
-            <PrivateRoute>
-              <WatchHistory />
-            </PrivateRoute>
-          } 
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Auth />} />
+      <Route path="/register" element={<Registration />} /> {/* Fixed syntax */}
+      <Route 
+        path="/Main" 
+        element={
+          <PrivateRoute>
+            <Main />
+          </PrivateRoute>
+        } 
+      />
+      <Route 
+        path="/profile" 
+        element={
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        } 
+      />
+      <Route 
+        path="/history" 
+        element={
+          <PrivateRoute>
+            <WatchHistory />
+          </PrivateRoute>
+        } 
+      />
+    </Routes>
   );
 };
 
